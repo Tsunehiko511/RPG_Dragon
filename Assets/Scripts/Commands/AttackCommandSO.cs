325f2af6fc17149425e3573862111eed76d6dafc
嶋津恒彦 <simanezumi1989@gmail.com>
2021-04-04 01:20:01 +0900

    コマンドの多様化

---
+++ Assets/Scripts/Commands/AttackCommandSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AttackCommandSO : CommandSO
+{
+    [SerializeField] int at;
+
+    // CommandSOのExecuteは実行せずに上書きして実行する
+    public override void Execute(Battler user, Battler target)
+    {
+        target.hp -= at;
+        Debug.Log($"{target.name}に{at}のダメージ:残りHP{target.hp}");
+    }
+}
