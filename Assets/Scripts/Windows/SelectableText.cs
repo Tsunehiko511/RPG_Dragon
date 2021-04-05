e66d95ddb059c7ee9fb7bf672abc7b77e5eba42b
嶋津恒彦 <simanezumi1989@gmail.com>
2021-04-05 23:55:32 +0900

    Selectableコンポーネントの解説

---
+++ Assets/Scripts/Windows/SelectableText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectableText : Selectable
+{
+    // 選択状態になった時に勝手に実行される関数
+    public override void OnSelect(BaseEventData eventData)
+    {
+        // base.OnSelect(eventData);
+        Debug.Log($"{gameObject.name}が選択された");
+    }
+
+    // 非選択状態になった時に勝手に実行される関数
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        // base.OnDeselect(eventData);
+        Debug.Log($"{gameObject.name}の選択が外れた");
+    }
+}
