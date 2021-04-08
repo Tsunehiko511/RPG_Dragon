4869b7177189ccb93d9b3b941a9a0af903227041
嶋津恒彦 <simanezumi1989@gmail.com>
2021-04-08 21:42:49 +0900

    呪文コマンドWindowの作成

---
+++ Assets/Scripts/Phases/ChooseSpellCommandPhase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseSpellCommandPhase : PhaseBase
+{
+    public override IEnumerator Execute(BattleContext battleContext)
+    {
+        yield return null;
+        // 呪文一覧を表示したい！
+        battleContext.windowBattleSpellCommand.gameObject.SetActive(true);
+
+        // TODO:呪文の効果を発動！
+    }
+}
